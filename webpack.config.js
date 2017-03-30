aaf33165409be157c3fcbeea72b7d99787933679
nanocloudx <nanocloudx@gmail.com>
2017-03-30 19:56:59 +0900

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
             'scss': 'vue-style-loader!css-loader!sass-loader',
             'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
           }
+          // other vue-loader options go here
         }
       },
       {
@@ -42,3 +43,13 @@ module.exports = {
     hints: false
   }
 };
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    })
+  ]);
+}
